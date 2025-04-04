d75e2c275c8e42f90d2a2dcf877794e78ce13436
amir4976 <amirhosain4976@gmail.com>
2025-04-04 22:49:59 +0330

    wave 7 of turning to ts

--- components/templates/mainPage/DotedTitle.tsx
+++ components/templates/mainPage/DotedTitle.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function DotedTitle({title:string}) {
+function DotedTitle({title}:{title:string}) {
   return (
     <div>
         <div className="flex items-center font-bold" >
